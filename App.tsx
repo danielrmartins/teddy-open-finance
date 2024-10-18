def15ff79ddf671b3d606041477473b4f772f336
Daniel <danielmartins10@gmail.com>
2024-10-18 12:02:04 -0300

    feat: add selected icons

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { ActivityIndicator, Text, View } from 'react-native';
 import { useFonts } from '@expo-google-fonts/inter';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
-import { Hamburger } from 'phosphor-react-native'
+import { HouseSimple, List, Minus, PencilSimple, Plus, SquaresFour, Trash, User } from 'phosphor-react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
@@ -13,7 +13,14 @@ export default function App() {
     <View>
       {!fontsLoaded ? <ActivityIndicator /> : (
         <>
-          <Hamburger size={32} color="black" weight="bold" /> 
+          <List size={24} color='gray' /> 
+          <Plus size={17} weight='bold' />
+          <PencilSimple size={20} weight='bold' />
+          <Trash size={20} weight='bold' color='red' />
+          <HouseSimple size={20} weight='fill' />
+          <User size={20} weight='fill' color='#EE7D46'/>
+          <SquaresFour size={20} weight='fill' />
+          <Minus size={20} weight='bold' color='red'/>
           <Text>Open up App.tsx to start working on your app!</Text>
           <StatusBar style="auto" />
         </>
