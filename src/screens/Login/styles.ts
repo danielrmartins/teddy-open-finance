a6be01af4278ad8cb77165b97d6e0b5b0d0c4e0f
Daniel <danielmartins10@gmail.com>
2024-10-18 16:36:31 -0300

    style: add `background-color`

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled(SafeAreaView)`
   align-items: center;
   justify-content: center;
   padding: 20px;
+  background-color: #F5F5F5;
 `;
 
 export const Title = styled.Text`
