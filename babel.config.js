01ee1e13ad6eefb574ba6649c89dcafddb6ede62
Daniel <danielmartins10@gmail.com>
2024-10-18 11:05:30 -0300

    fix: babel root and alias

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,18 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [ "module-resolver", {
-      "root": ["./src"],
-      "alias": {
-        "@components": "./src/components",
-        "@screens": "./src/screens",
-        "@assets": "./src/assets",
-        "@storage": "./src/storage",
-        "@utils": "./src/utils",
-        "@routes": "./src/routes",
-      }
-    }]
+    plugins: [
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+          '@routes': './src/routes',
+        },
+      }],
+    ],
   };
 };
