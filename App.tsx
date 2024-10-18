d6162303833ade3d03c48bbb65921a4e4cab5882
Daniel <danielmartins10@gmail.com>
2024-10-18 10:52:49 -0300

    chore: add font to `App.tsx`

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { useFonts } from '@expo-google-fonts/inter';
+import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+
   return (
     <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      { !fontsLoaded ? <ActivityIndicator /> : (
+        <>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </>
+      )} 
     </View>
   );
 }
