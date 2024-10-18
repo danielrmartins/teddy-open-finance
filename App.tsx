67df51bd212aaa8e4b90d61ab07c979ca485e32d
Daniel <danielmartins10@gmail.com>
2024-10-18 15:01:56 -0300

    chore: add `ThemeProvider`

--- App.tsx
+++ App.tsx
@@ -3,16 +3,18 @@ import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, Text, View } from 'react-native';
 import { useFonts } from '@expo-google-fonts/inter';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
-
 import { HouseSimple, List, Minus, PencilSimple, Plus, SquaresFour, Trash, User } from 'phosphor-react-native'
+import { ThemeProvider } from 'styled-components/native';
 import { Routes } from './src/routes';
+
+import theme from 'src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
   return (
-    <>
-      <StatusBar translucent backgroundColor='transparent'/>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="auto" />
       {fontsLoaded ? <Routes /> : <ActivityIndicator />} 
           {/* <List size={24} color='gray' /> 
           <Plus size={17} weight='bold' />
@@ -23,6 +25,6 @@ export default function App() {
           <SquaresFour size={20} weight='fill' />
           <Minus size={20} weight='bold' color='red'/>
           <Text>Open up App.tsx to start working on your app!</Text> */}
-    </>
+    </ThemeProvider>
   );
 }
