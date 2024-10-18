4d9b0e56af778a20283ffc400b159e1a53a7bbe1
Daniel <danielmartins10@gmail.com>
2024-10-18 11:27:14 -0300

    feat: add login screen and icon just for test

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import { ActivityIndicator, Text, View } from 'react-native';
 import { useFonts } from '@expo-google-fonts/inter';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
+import { Hamburger } from 'phosphor-react-native'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
   return (
     <View>
-      { !fontsLoaded ? <ActivityIndicator /> : (
+      {!fontsLoaded ? <ActivityIndicator /> : (
         <>
+          <Hamburger size={32} color="black" weight="bold" /> 
           <Text>Open up App.tsx to start working on your app!</Text>
           <StatusBar style="auto" />
         </>
