1e13713257660e75515a92532590cfcb72336c5e
Daniel <danielmartins10@gmail.com>
2024-10-18 16:38:23 -0300

    feat: create `Card` component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 16px;
+  margin-bottom: 16px;
+`;
