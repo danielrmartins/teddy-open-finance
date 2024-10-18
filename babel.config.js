12863732a83129e2ea7f448a633803805c6e2606
Daniel <danielmartins10@gmail.com>
2024-10-18 10:27:50 -0300

    chore: configure alias and paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [ "module-resolver", {
+      "root": ["./src"],
+      "alias": {
+        "@components": "./src/components",
+        "@screens": "./src/screens",
+        "@assets": "./src/assets",
+        "@storage": "./src/storage",
+        "@utils": "./src/utils",
+        "@routes": "./src/routes",
+      }
+    }]
   };
 };
