4679b8e41f18de504bf3d278ea25a4d9a78c0a49
Daniel <danielmartins10@gmail.com>
2024-10-19 12:17:18 -0300

    feat: add `Card` component do clients screen

--- src/screens/Clients/index.tsx
+++ src/screens/Clients/index.tsx
@@ -1,42 +1,69 @@
 import { List } from "phosphor-react-native";
-import { Text } from "react-native";
-
-import { Container, Header, Logo } from "./styles";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import logoImg from '@assets/teddy-logo.png';
-import { useEffect, useState } from "react";
 import { api } from "@services/api";
+import { Card } from "@components/Card";
+// import DrawerModal from "@components/Drawer";
 
+import { Container, Header, Logo } from "./styles";
+
+
+export interface IClient {
+  id: string;
+  name: string;
+  salary: number;
+  companyValuation: number;
+}[]
 
 export function Clients() {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(1);
+  const [clients, setClients] = useState<IClient[]>([]);
+  const [loading, setLoading] = useState(true);
+  // const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
-    console.log('Clients screen');
-
     async function fetchClients() {
       try {
-        const response = await api.get('/users', { params: { page, limit }});
-        console.log(response.data);
+        const { data } = await api.get('/users', { params: { page, limit }});
+        
+        setClients(data.clients);
       } catch (error) {
         console.error('Error fetching clients:', error);
+      } finally {
+        setLoading(false);
       }
     }
 
     fetchClients();
   }, [page, limit]);
 
+  const renderClients = () => {
+    if (loading) {
+      return <ActivityIndicator />
+    }
+
+    return clients.map(client => (
+      <Card key={client.id} client={client} />
+    ));
+  }
+
   return (
     <SafeAreaView>
       <Header>
-         <Logo source={logoImg}/>
+        <Logo source={logoImg}/>
+        {/* <TouchableOpacity onPress={() => setIsModalVisible(true)}> */}
         <List size={24} color='gray' />
+        {/* </TouchableOpacity> */}
       </Header>
       <Container>
-        <Text>dsadsadasdsadsa</Text>
+      {loading ? <ActivityIndicator /> : <Text>{`${clients.length} ${clients.length === 1 ? 'cliente encontrado' : 'clientes encontrados'}`}</Text>}
+      {renderClients()}
       </Container>
+      {/* <DrawerModal visible={isModalVisible} onClose={() => setIsModalVisible(false)} /> */}
     </SafeAreaView>
   )
 }
