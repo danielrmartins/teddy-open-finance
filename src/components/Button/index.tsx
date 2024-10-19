cf2be3d64b4e39e5ae0ed3b0573d39d54a08730f
Daniel <danielmartins10@gmail.com>
2024-10-19 14:30:36 -0300

    chore: add `text` to `Button`

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,13 @@ import { Container, Text } from "./styles";
 type ButtonProps = {
   type?: "primary" | "secondary";
   onPress: () => void;
+  text: string;
 };
 
-export function Button({ type = 'primary', onPress }: ButtonProps) {
+export function Button({ type = 'primary', onPress, text }: ButtonProps) {
   return (
     <Container type={type} onPress={onPress}>
-      <Text type={type}>Entrar</Text>
+      <Text type={type}>{text}</Text>
     </Container>
   );
 }
