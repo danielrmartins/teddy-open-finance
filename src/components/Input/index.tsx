ed1f174ffad18800d6d7a046ca8619ba5e2c7885
Daniel <danielmartins10@gmail.com>
2024-10-19 18:09:22 -0300

    feat: finish `Input` component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,15 +3,21 @@ import { InputContainer, TextInput, Title } from "./styles";
 interface InputProps {
   placeholder: string;
   onChangeText: (text: string) => void;
-  value: string;
+  value: string | undefined;
   title: string;
+  keyboardType?: "default" | "number-pad";
 }
 
-export function Input({ placeholder, onChangeText, value, title }: InputProps) {
+export function Input({ placeholder, onChangeText, value, title, keyboardType }: InputProps) {
   return (
     <InputContainer>
       <Title>{title}</Title>
-      <TextInput placeholder={placeholder} onChangeText={onChangeText} value={value} />
+      <TextInput 
+        placeholder={placeholder} 
+        onChangeText={onChangeText} 
+        value={value} 
+        keyboardType={keyboardType}
+      />
     </InputContainer>
   );
 }
